5978e35280bab7394134f1365c7251fb08ac9c77
saeed <01shadowalker01@gmail.com>
2019-09-29 13:43:36 +0330

    init (library generated application for showcase generated)

---
+++ projects/shw-dynamic-table/src/lib/shw-dynamic-table.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'lib-shw-dynamic-table',
+  template: `
+    <p>
+      shw-dynamic-table works!
+    </p>
+  `,
+  styles: []
+})
+export class ShwDynamicTableComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
